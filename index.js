8f5dc42c11abba8edf3228a88f178c9e689c0166
Saklainmostak-learner <saqlainmustaksaqi1@gmail.com>
2026-05-22 08:08:39 +0600

    finalize backend api and jwt protected routes

--- index.js
+++ index.js
@@ -1,17 +1,214 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-vrrrnum-shard-00-00.ptiwbzi.mongodb.net:27017,ac-vrrrnum-shard-00-01.ptiwbzi.mongodb.net:27017,ac-vrrrnum-shard-00-02.ptiwbzi.mongodb.net:27017/?ssl=true&replicaSet=atlas-gj74l1-shard-0&authSource=admin&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
+
+async function run() {
+  try {
+    const db = client.db("sportnest");
+
+    const facilitiesCollection = db.collection("facilities");
+    const bookingsCollection = db.collection("bookings");
+
+    // JWT
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "strict",
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
+          sameSite: "strict",
+        })
+        .send({ success: true });
+    });
+
+    // GET ALL FACILITIES
+    app.get("/facilities", async (req, res) => {
+      const search = req.query.search || "";
+      const type = req.query.type || "";
+
+      const query = {
+        name: {
+          $regex: search,
+          $options: "i",
+        },
+      };
+
+      if (type && type !== "All Sports") {
+        query.type = type;
+      }
+
+      const result = await facilitiesCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    // GET SINGLE FACILITY
+    app.get("/facilities/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await facilitiesCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send(result);
+    });
+
+    // ADD FACILITY
+    app.post("/facilities", verifyToken, async (req, res) => {
+      const facility = req.body;
+
+      const result = await facilitiesCollection.insertOne(facility);
+
+      res.send(result);
+    });
+
+    // UPDATE FACILITY
+    app.patch("/facilities/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+
+      const updatedFacility = req.body;
+
+      const result = await facilitiesCollection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        {
+          $set: updatedFacility,
+        }
+      );
+
+      res.send(result);
+    });
+
+    // DELETE FACILITY
+    app.delete("/facilities/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+
+      const result = await facilitiesCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send(result);
+    });
+
+    // GET BOOKINGS
+    app.get("/bookings", verifyToken, async (req, res) => {
+      const email = req.query.email;
+
+      if (req.user.email !== email) {
+        return res.status(403).send({
+          message: "Forbidden access",
+        });
+      }
+
+      const result = await bookingsCollection
+        .find({
+          userEmail: email,
+        })
+        .toArray();
+
+      res.send(result);
+    });
+
+    // CREATE BOOKING
+    app.post("/bookings", verifyToken, async (req, res) => {
+      const booking = req.body;
+
+      booking.status = "pending";
+
+      const result = await bookingsCollection.insertOne(booking);
+
+      res.send(result);
+    });
+
+    // CANCEL BOOKING
+    app.patch("/bookings/:id/cancel", verifyToken, async (req, res) => {
+      const id = req.params.id;
+
+      const result = await bookingsCollection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        {
+          $set: {
+            status: "cancelled",
+          },
+        }
+      );
+
+      res.send(result);
+    });
+
+    console.log("SportNest MongoDB Connected");
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("SportNest server is running");
+  res.send("SportNest Server Running");
 });
 
 app.listen(port, () => {
-  console.log(`SportNest server running on port ${port}`);
+  console.log(`SportNest running on port ${port}`);
 });
